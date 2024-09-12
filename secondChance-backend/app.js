e1008225af85077eb874e21328398ebeb4fd7ee6
Mauro <machado.mauroc@gmail.com>
2024-09-12 20:45:46 +0200

    Update app.js
    Linted file

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,23 +1,21 @@
-/*jshint esversion: 8 */
+/* jshint esversion: 8 */
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const pinoLogger = require('./logger')
 const path = require('path')
 const connectToDatabase = require('./models/db')
-const {loadData} = require("./util/import-mongo/index")
-
+const { loadData } = require('./util/import-mongo/index')
 
 const app = express()
-app.use("*",cors())
+app.use('*', cors())
 const port = 3060
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB')
+  pinoLogger.info('Connected to DB')
 })
-    .catch((e) => console.error('Failed to connect to DB', e))
-
+  .catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json())
 
